0b94c5289117040d1ca5cebb15e03ad3639a4d27
Timmy O'Mahony <hey@timmyomahony.com>
2023-06-28 11:37:23 +0100

    Updated casing

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import "../styles/index.css";
 
 const NotFound = () => {
   return (
-    <main className="bg-beige fixed w-full h-full flex justify-center items-center">
+    <main className="fixed w-full h-full flex justify-center items-center">
       <article>
         <a href="/" className="text-7xl underline">
           404
