16ce12fa96f09b923464c9947c2712c87542427f
Timmy O'Mahony <hey@timmyomahony.com>
2023-07-04 15:38:09 +0100

    Added logo to header

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { sendEmail, getEmailAddress } from "../utils/email";
-
 import Link from "next/link";
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import LinkedIcon from "../icons/linkedin.svg";
 import TwitterIcon from "../icons/twitter.svg";
+import LogoLight from "../images/logo-light.png";
 
 import Container from "./Container";
 
@@ -14,13 +13,22 @@ export default function Nav() {
     <Container>
       <nav className="w-full flex justify-between items-center py-10 lg:py-12 border-b-2 border-zinc-800">
         <p className="text-lg lg:text-xl 2xl:text-2xl">
-          <Link href="/" className="hover:underline">
-            Founder Roadmap
+          <Link href="/" className="hover:underline flex items-center gap-4">
+            <Image
+              src={LogoLight}
+              alt="Founder Roadmap"
+              className="h-6 w-auto"
+              placeholder="blur"
+              quality={100}
+              priority
+            />
+            <span>Founder Roadmap</span>
           </Link>
         </p>
-        <ul className="flex gap-3 text-zinc-600">
+        <ul className="flex gap-3 ">
           <li className="w-4 h-4 lg:w-5 lg:h-5">
             <a
+              className="text-zinc-600 hover:text-white transition-all duration-300 ease-linear"
               href="https://www.linkedin.com/company/founder-roadmap/"
               title="Connect on LinkedIn"
               target="_blank"
@@ -31,8 +39,9 @@ export default function Nav() {
           </li>
           <li className="w-4 h-4 lg:w-5 lg:h-5">
             <a
+              className="text-zinc-600 hover:text-white transition-all duration-300 ease-linear"
               href="https://twitter.com/founderroadmap/"
-              title="Follow me on Twitter"
+              title="Follow on Twitter"
               target="_blank"
               rel="noreferrer"
             >
